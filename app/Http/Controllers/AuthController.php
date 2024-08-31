c53da2546a901f5f8ac823bf48fcee74de7972a0
Anthony Kusuma <hi@anthonykusuma.com>
2024-08-31 20:22:48 +0800

    feat: add user authentication route

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a new user.
+     */
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)->letters()->mixedCase()->numbers()->symbols(),
+            ],
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken($request->email);
+
+        $response = [
+            'user' => $user,
+            'token' => $token->plainTextToken,
+        ];
+
+        return response($response, 201);
+    }
+
+    /**
+     * Logs user into the system
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => 'Invalid credentials.',
+            ], 401);
+        }
+
+        $token = $user->createToken($request->email);
+
+        $response = [
+            'user' => $user,
+            'token' => $token->plainTextToken,
+        ];
+
+        return response($response, 201);
+    }
+
+    /**
+     * Revokes the current user's token
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->noContent();
+    }
+
+    /**
+     * Revokes all of the current user's tokens
+     */
+    public function logoutAllDevices(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->noContent();
+    }
+}
