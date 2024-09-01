6e50a491d614c93a324d1e3cf4f37bb7cfb8df78
Anthony Kusuma <hi@anthonykusuma.com>
2024-09-01 14:31:11 +0800

    feat: add post factory and database seeder for demo purpose

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Anthony',
+            'email' => 'hi@anthonykusuma.com',
+            'password' => bcrypt('Pswd1234!'),
         ]);
+
+        Post::factory(18)->create();
     }
 }
