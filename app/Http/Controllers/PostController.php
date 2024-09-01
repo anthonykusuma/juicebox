42a6d86a23b71c1b5bd8f19ad6a568739b0c26dc
Anthony Kusuma <hi@anthonykusuma.com>
2024-09-02 00:10:45 +0800

    chore(post): utilise resource for scramble documentation - index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,23 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
     /**
-     * Display a paginated list of all posts.
+     * List all posts
+     *
+     * Return a list of posts.
+     *
+     * @response AnonymousResourceCollection<LengthAwarePaginator<PostResource>>
+     *
+     * @unauthenticated
      */
     public function index(Request $request)
     {
-        $posts = Post::paginate(10);
+        $perPage = $request->integer('per_page', 10);
+        $page = $request->integer('page', 1);
 
-        return response($posts, 200);
+        $posts = Post::paginate($perPage, ['*'], 'page', $page);
+
+        return PostResource::collection($posts);
     }
 
     /**
@@ -31,11 +43,14 @@ class PostController extends Controller
             'content' => 'required',
         ]);
 
-        $post = $request->user()->posts()->create([
-            'title' => $fields['title'],
-            'slug' => $this->generateUniqueSlug($fields['title']),
-            'content' => $fields['content'],
-        ]);
+        $post = $request
+            ->user()
+            ->posts()
+            ->create([
+                'title' => $fields['title'],
+                'slug' => $this->generateUniqueSlug($fields['title']),
+                'content' => $fields['content'],
+            ]);
 
         return response($post, 201);
     }
@@ -43,14 +58,16 @@ class PostController extends Controller
     /**
      * Display the specified post.
      *
-     * @param  int  $id  the postID
+     * @param  int  $id  The post ID
+     *
+     * @unauthenticated
      */
     public function show($id)
     {
         try {
             $post = Post::findOrFail($id);
 
-            return response($post, 200);
+            return response(PostResource::make($post), 200);
         } catch (ModelNotFoundException $e) {
             return response()->json(['error' => 'Post not found'], 404);
         }
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
